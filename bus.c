725b88c73a3be56e1f6f5b98b7536ce8f542e4bb
jdaridis <sdi1700028@di.uoa.gr>
2019-12-06 16:51:35 +0200

    Made config file. Started implementing station_manager from pseudocode. Made bus.c and .h

---
+++ bus.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/shm.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <wait.h>
+#include "bus.h"
+
+int main(int argc, char const *argv[]){
+    /* code */
+    return 0;
+}
---
+++ bus.h
@@ -0,0 +1,8 @@
+enum bus_types{VOR,ASK,PEL};
+
+typedef struct bus{
+    int type;
+    int people;
+    int capacity;
+    int parkperiod;
+} bus;
